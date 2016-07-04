1299f6888a4c34637423d803e65a56e4be5d89d6
root <bonelesshamu>
2016-07-05 08:20:32 +0900

    6th finished

---
+++ spec/experiments/string_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe String do
+  describe '#<<' do
+    example 'adding string' do
+      pending('not implement')
+      s = "ABC"
+      s << "0"
+      expect(s.size).to eq(4)
+    end
+  end
+end
