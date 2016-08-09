16694e7a90494746d7341aa4e31a6786474086c9
root <bonelesshamu>
2016-08-09 22:44:00 +0900

    login&logout staff

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -7,4 +7,23 @@ class Admin::SessionsController < Admin::Base
       render action: 'new'
     end
   end
+
+  def create
+    @form = Admin::LoginForm.new(params[:admin_login_form])
+    if @form.email.present?
+      admin_member = Administrator.find_by(email_for_index: @form.email.downcase)
+    end
+    if admin_member
+      session[:administrator_id]=admin_member.id
+      redirect_to :admin_root
+    else
+      render action: 'new'
+    end
+  end
+
+  def destroy
+    session.delete(:administrator_id)
+    redirect_to :admin_root
+  end
+
 end
